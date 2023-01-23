9562dfc65f8f47c82ccb1f55d99b0810648a1ab1
K9H9 <dev>
2023-01-23 13:08:04 +0200

    Fixing require path

--- plugins/init.lua
+++ plugins/init.lua
@@ -1,14 +1,21 @@
+
 return {
   ["Exafunction/codeium.vim"] = {},
   ["Pocco81/true-zen.nvim"] = {
     config = function()
-      require("true-zen").setup {}
+      require("true-zen").setup {
+        modes = {
+          ataraxis = {
+            shade = "dark",
+          }
+        }
+      }
     end,
   },
   ['testaustime/testaustime.nvim'] = {
     requires = { 'nvim-lua/plenary.nvim' },
     config = function()
-      dofile("/home/koho/code/neovim/plugins/testaustime/token.lua")
+      require("custom.pluginss.testaustime")
     end
   },
 }
